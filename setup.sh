5e8e92e5b40f545af010db8676b42c3daf598941
Jacob Dubinsky <jacob.dubinsky@shopify.com>
2021-05-03 15:29:08 -0400

    setup.sh: vim-plug install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# install required packages
 sudo apt-get install -y neovim
 
 if ! command -v rg &> /dev/null; then
@@ -10,10 +11,17 @@ if ! command -v fzf &> /dev/null; then
   sudo apt-get install -y fzf
 fi
 
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# create dir structure for neovim
 mkdir -p ~/.config/nvim/
 
+# dotfiles
 ln -sf ~/dotfiles/mackup/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/mackup/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/mackup/.zshrc ~/.zshrc
 
+# install neovim plugins
 nvim --headless +PlugInstall +qall
