82588e4e0a832d49fd75c6c52f489c494c08c0db
Jacob Dubinsky <jacob.dubinsky@shopify.com>
2021-05-03 15:50:17 -0400

    update zshrc and install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ if ! command -v fzf &> /dev/null; then
   sudo apt-get install -y fzf
 fi
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
