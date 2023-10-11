1528d27a7965c845c5b36f187f4ab375451fbe27
Jacob Dubinsky <jacob.dubinsky@shopify.com>
2023-10-11 10:34:24 -0400

    lazygit

--- setup.sh
+++ setup.sh
@@ -22,6 +22,12 @@ if ! command -v rg &> /dev/null; then
   sudo apt-get install -y fd
 fi
 
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 sudo apt install -y fd-find
 
 FZF_VERSION=0.35.1
