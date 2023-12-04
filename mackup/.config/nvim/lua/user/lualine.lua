21b2ae036f9336a87fa57c1e97d4a4d2f729b7ef
Jacob Dubinsky <jacob.dubinsky@shopify.com>
2023-12-04 11:37:12 -0500

    inactive filename path lualine

--- mackup/.config/nvim/lua/user/lualine.lua
+++ mackup/.config/nvim/lua/user/lualine.lua
@@ -7,6 +7,9 @@ lualine.setup({
   sections = {
     lualine_c = { { "filename", path = 1 } },
   },
+  inactive_sections = {
+    lualine_c = { { "filename", path = 1 } },
+  },
 	options = {
 		theme = "dracula-nvim",
 	},
